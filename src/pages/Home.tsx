66f753bcab5d3f13a416dac886849b7bf720bc4b
minh-tri <cmtri2601@gmail.com>
2024-12-13 17:04:47 +0700

    config shadcn

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Button } from '~/pages/components/ui/button';
+
+const Home = () => {
+  return (
+    <div>
+      <Button>Click me</Button>Home
+    </div>
+  );
+};
+
+export default Home;
