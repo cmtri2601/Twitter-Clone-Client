a6057c1adbc147759e591ee36d684f78a6539ad2
minh-tri <cmtri2601@gmail.com>
2025-03-09 23:46:30 +0700

    Fix bug useEffect oauth

--- src/pages/OAuth.tsx
+++ src/pages/OAuth.tsx
@@ -1,18 +1,19 @@
+import { useEffect, useRef } from 'react';
 import { Navigate, useParams, useSearchParams } from 'react-router-dom';
 import { toast } from 'sonner';
 import { useAuth } from '~/components/auth/auth-provider';
 import { StorageKey } from '~/constants/StorageKey';
-import { useDidUpdateEffect } from '~/hooks/useDidUpdateEffect';
 
 const OAuth = () => {
   const params = useParams();
   const [searchParams] = useSearchParams();
+  const toastId = useRef<string | number | null>(null);
 
   // Auth
   const { setAuth } = useAuth();
 
   // Handle login by oauth
-  useDidUpdateEffect(() => {
+  useEffect(() => {
     localStorage.setItem(
       StorageKey.ACCESS_TOKEN,
       searchParams.get('access_token') as string
@@ -25,8 +26,16 @@ const OAuth = () => {
     // set auth
     setAuth({ user: JSON.parse(searchParams.get('user') as string) });
 
-    // toast
-    toast.success(`Successfully login with ${params.resourceServer}`);
+    // handle dismiss toast
+    if (toastId.current) toast.dismiss(toastId.current);
+
+    // clean up fn
+    return () => {
+      toastId.current = toast.success(
+        `Successfully login with ${params.resourceServer}`
+      );
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return <Navigate to={'/'} />;
